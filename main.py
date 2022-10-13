106de67f4843e18a6203069ea6ff35bd5551d6e7
arsanav <arsanav@gmail.com>
2022-10-13 08:58:13 +0300

    comments

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ import time
 import requests
 import json
 
+
+# Выкачка данных из групп тематики Big Data
 def posts_search_vk_api_groups():
+    # Токен для подключения к VK API
     token = 'a92f413ba92f413ba92f413b39aa3f8a72aa92fa92f413bca04bf22e2703f30a8ec4c6f'
     version = 5.131
+
+    # Список доменов групп для выкачки
     domains = ['mashinnoe_obuchenie_ai_big_data', 'datascience_ai', 'datascience', 'datamining.team']
+
+    # Количество выкачиваемых записей за итерацию (у VK API лимит до 100)
     count = 100
     posts = []
 
     for domain in domains:
+        # Смещение для обхода лимита
         offset = 0
         while offset < 2500:
+            # Запрос wall.get
             response = requests.get('https://api.vk.com/method/wall.get',
                                     params={
                                         'access_token': token,
@@ -30,6 +39,8 @@ def posts_search_vk_api_groups():
 
 
 result = posts_search_vk_api_groups()
+
+# Формирование json файла
 with open('vk_dump.json', 'w') as outfile:
     json.dump(result, outfile, indent=4)
-print(result)
+
